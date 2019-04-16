05755fb8fd550dbf32883c837370efecbcf75bf1
Wyatt <wyatt.troutman@gmail.com>
2019-04-16 09:00:56 -0400

    Setup links in unit test project

---
+++ Data-Structures-Tests/Tests/LinkedList/LinkedListUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data_Structures_Tests.Tests.LinkedList
+{
+    [TestClass]
+    public class LinkedListUnitTest
+    {
+        [TestMethod]
+        public void TestInitialization()
+        {
+            Console.WriteLine("Testing unit test");
+        }
+    }
+}
